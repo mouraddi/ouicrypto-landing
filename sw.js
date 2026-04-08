bfb7ba3f45fe09f3de7507221f1c7ecbe1bd8e11
mouraddi <94260095+mouraddi@users.noreply.github.com>
2026-04-09 00:49:58 +0100

    Update service worker script import with new zoneId

--- sw.js
+++ sw.js
@@ -1,6 +1,1 @@
-self.options = {
-    "domain": "5gvci.com",
-    "zoneId": 10850402
-}
-self.lary = ""
-importScripts('https://5gvci.com/act/files/service-worker.min.js?r=sw')
+importScripts('https://5gvci.com/act/files/service-worker.min.js?z=10851632');
